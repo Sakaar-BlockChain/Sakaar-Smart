09383e330e6863dd4ba4b0d88df6d4c0600b198c
StiveMan1 <04024004zh@gmail.com>
2023-02-06 17:54:09 +0300

    -errors

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -72,11 +72,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/stiveman1/CLionProjects/Sakaar-SmartContract/cmake-build-debug/lib/Sakaar-NetWork/cmake_install.cmake")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/stiveman1/CLionProjects/Sakaar-SmartContract/cmake-build-debug/lib/Sakaar/cmake_install.cmake")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
